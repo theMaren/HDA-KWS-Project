9c750c5d0c788a9d735efc4db436f3d0c1d3be0e
theMaren <56018058+theMaren@users.noreply.github.com>
2024-02-17 14:31:01 +0100

    Make pretty

--- evaluation.py
+++ evaluation.py
@@ -10,14 +10,14 @@ import subprocess
 import psutil
 
 
-
+#get the cpu usage (for google colab environment)
 def get_system_ram_usage():
     ram_info = psutil.virtual_memory()
     used_ram = ram_info.used / (1024 ** 3)# Convert bytes to GB
 
     return used_ram
 
-
+#log the gpu usage every interval seconds (for google colab environment)
 def log_gpu_usage(log_file_path, stop_event, interval):
     with open(log_file_path, 'a') as log_file:
         while not stop_event.is_set():
@@ -28,11 +28,13 @@ def log_gpu_usage(log_file_path, stop_event, interval):
             time.sleep(interval)
 
 
+#read the gpu usage from the log file, plot it and put metrics tp dataframe
 def get_gpu_usage(logfile_path):
 
     with open(logfile_path, 'r') as file:
         log_data = file.read()
 
+    #extract gpu pattern from logfile
     gpu_util_pattern = re.compile(r'\|\s*(\d+)%\s+Default')
     memory_usage_pattern = re.compile(r'\|\s+(\d+)MiB / (\d+)MiB')
 
@@ -45,7 +47,7 @@ def get_gpu_usage(logfile_path):
         'Memory Usage %': [round((int(usage[0]) / int(usage[1])) * 100, 3) for usage in memory_usages]
     })
 
-    plt.plot(df.index, df['Memory Usage (MiB)'], marker='x', color='darkred')  # Use 'darkred' for the plot color
+    plt.plot(df.index, df['Memory Usage (MiB)'], marker='x', color='darkred')
     plt.title('GPU Memory Usage (MIB)')
     plt.xlabel('Time (Arbitrary Units)')
     plt.ylabel('Memory Usage (MIB)')
@@ -56,6 +58,7 @@ def get_gpu_usage(logfile_path):
 
     return df
 
+#return dataframe of common error metrics
 def get_error_metrics(model_name, true_labels, y_pred_logits):
 
     predicted_classes = tf.argmax(y_pred_logits, axis=1)
@@ -69,7 +72,7 @@ def get_error_metrics(model_name, true_labels, y_pred_logits):
     cross_entropy_loss = loss_fn(true_labels, y_pred_logits).numpy()
 
     metrics_data = {
-        'Model Name': [model_name],  # This creates a single-row DataFrame
+        'Model Name': [model_name],
         'Precision': [precision],
         'Recall': [recall],
         'F1 Score': [f1],
@@ -82,10 +85,11 @@ def get_error_metrics(model_name, true_labels, y_pred_logits):
 
     return metrics_df
 
+#return the class name for numeric label
 def numeric_to_string_labels(vocabulary, numeric_labels):
     return np.array(vocabulary)[numeric_labels]
 
-
+#plot confusion matrix for a given prediction and true labels combination
 def plot_confusion_matrix(true_labels, y_pred_logits,label_lookup):
 
     predicted_classes = tf.argmax(y_pred_logits, axis=1)
@@ -107,6 +111,7 @@ def plot_confusion_matrix(true_labels, y_pred_logits,label_lookup):
     plt.show()
 
 
+#plot the training history of a model
 def plot_fit_history(history, loss_columns, accuracy_columns):
     plt.figure(figsize=(12, 6))
 
